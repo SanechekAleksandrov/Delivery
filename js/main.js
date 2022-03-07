488673995cbbe3724ba6c7182deb0519578e09a7
SanechekAleksandrov <sanechek-1994@mail.ru>
2022-03-07 04:28:16 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+   $('.services__wrapper-inner').slick({
+      autoplay:true,
+      autoplaySpeed: 3000,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/block__four/slider/arrow-left.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/block__four/slider/arrow-right.svg" alt=""></button>'
+
+   });
+});
